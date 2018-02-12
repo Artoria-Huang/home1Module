9638cf89e8aa56d475f9f133fa4cc2887f92e734
“huangjing” <huangjing@rong360.com>
2018-02-12 17:35:42 +0800

    init spec

---
+++ Example/Pods/Target Support Files/Pods-home1Module_Tests/Pods-home1Module_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_home1Module_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_home1Module_Tests
+@end
---
+++ Example/Pods/Target Support Files/home1Module/home1Module-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_home1Module : NSObject
+@end
+@implementation PodsDummy_home1Module
+@end
